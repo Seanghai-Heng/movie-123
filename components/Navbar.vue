885d67c9a31da7167a2e938c161af26502f1eecb
Seanghai HENG <seanghai.heng@allweb.com.kh>
2022-09-07 14:23:24 +0700

    prevent page from reload when search

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,28 +1,56 @@
 <template>
   <div>
-<nav class="topnav navbar-dark navbar navbar-expand-sm text-white pb-4">
-   <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse navbar-default" id="navbarCollapse">
-   <router-link to="/">Home</router-link> 
-      <router-link to="/about">About</router-link> 
-      <router-link to="#">TV Shows</router-link> 
+    <nav class="topnav navbar-dark navbar navbar-expand-sm text-white pb-4">
+      <button
+        type="button"
+        class="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse navbar-default" id="navbarCollapse">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="#">TV Shows</router-link>
       </div>
-  <div class="search-container">
-    <form autocomplete="off" action="/search">
-      <input type="text" placeholder="TV Show" name="tvShow">
-      <button type="submit"><i class="fa fa-search"></i></button>
-    </form>
-  </div>
-</nav>
+      <div class="search-container">
+        <form autocomplete="off" @submit.stop.prevent="submit">
+          <input
+            type="text"
+            placeholder="TV Show"
+            name="tvShow"
+            v-model="tvShow"
+          />
+          <button type="submit" @click="$nuxt.$emit('search', tvShow)">
+            <i class="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
 
-
+<script>
+export default {
+  name: "movie",
+  data() {
+    return {
+      tvShow: null,
+    };
+  },
+  methods: {
+    submit() {
+      this.$router.push("/search?tvShow=" + this.tvShow);
+    },
+  },
+};
+</script>
 
 <style>
-* {box-sizing: border-box;}
+* {
+  box-sizing: border-box;
+}
 
 body {
   margin: 0;
@@ -49,7 +77,7 @@ body {
 }
 
 .topnav a.active {
-  background-color: #2196F3;
+  background-color: #2196f3;
   color: white;
 }
 
@@ -57,7 +85,7 @@ body {
   float: right;
 }
 
-.topnav input[type=text] {
+.topnav input[type="text"] {
   padding: 6px;
   margin-top: 8px;
   font-size: 17px;
@@ -76,7 +104,7 @@ body {
   cursor: pointer;
 }
 input:focus {
-    border: none;
+  border: none;
 }
 .topnav .search-container button:hover {
   background: #ccc;
@@ -86,29 +114,28 @@ input:focus {
   .topnav .search-container {
     float: none;
   }
-  .navbar-toggler{
+  .navbar-toggler {
     width: 100%;
     text-align: left;
   }
-  .search-container{
-    width:100%;
+  .search-container {
+    width: 100%;
   }
   .topnav .search-container button {
     width: 10%;
     text-align: left;
-    display:block;
+    display: block;
     float: none;
-    
   }
-  .topnav a{
+  .topnav a {
     padding: 5px;
     margin-right: 20px;
     display: block;
     text-align: left;
-    float : none;
-    padding:14px;
+    float: none;
+    padding: 14px;
   }
-   .topnav input[type=text]{
+  .topnav input[type="text"] {
     float: none;
     display: block;
     text-align: left;
@@ -116,8 +143,8 @@ input:focus {
     margin: 0;
     padding: 14px;
   }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;  
+  .topnav input[type="text"] {
+    border: 1px solid #ccc;
   }
 }
 </style>
