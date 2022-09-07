8c08c1268b6fe092d56c1239cf3103d0c2dc673d
Seanghai HENG <seanghai.heng@allweb.com.kh>
2022-09-07 15:08:10 +0700

    show error when no result found

--- pages/Search.vue
+++ pages/Search.vue
@@ -66,6 +66,10 @@
         </div>
       </div>
     </div>
+    <div class="pb-4"></div>
+    <div class="flex pb-5 mx-auto px-3pt-5 
+            border-t border-gray-500 text-gray-400 text-sm 
+            flex-col md:flex-row max-w-6xl"></div>
   </div>
 </template>
 
