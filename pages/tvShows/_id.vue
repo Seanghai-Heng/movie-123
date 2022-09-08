2ccb2fbf7dd75409cbd5ee6ae60e899c4fcbd1d6
Seanghai HENG <seanghai.heng@allweb.com.kh>
2022-09-08 11:33:47 +0700

    add minutes to runtime

--- pages/tvShows/_id.vue
+++ pages/tvShows/_id.vue
@@ -149,7 +149,7 @@
                   <p><span class="fw-bold">Episode's Name: </span> {{ episode.name }} </p>
                   <p><span class="fw-bold">Episode's Type: </span> {{ episode.type }} </p>
                   <p><span class="fw-bold">Air Date: </span> {{ episode.airdate }} </p>
-                  <p><span class="fw-bold">Run Time: </span> {{ episode.runtime }} </p>
+                  <p><span class="fw-bold">Run Time: </span> {{ episode.runtime }} minutes</p>
                   <p><span class="fw-bold">Rating: </span> {{ episode.rating.average??"null" }} </p>
               </div>
             </div>
