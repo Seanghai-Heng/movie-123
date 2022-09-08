3409403829f75e4443a76bf42dcef048a0f0d7f5
Seanghai HENG <seanghai.heng@allweb.com.kh>
2022-09-08 11:46:20 +0700

    fix

--- pages/tvShows/_id.vue
+++ pages/tvShows/_id.vue
@@ -91,9 +91,12 @@
             </div>
           </div>
         </div>
-        <div v-else class="container">
+        <div v-else class="container pb-4">
           <h1 style="font-size: 50px">No Casts To Display</h1>
         </div>
+        <div
+      class="flex pb-5 mx-auto px-3pt-5 border-t border-gray-500 text-gray-400 text-sm flex-col md:flex-row max-w-6xl"
+    ></div>
 
         <!-- Episodes Section -->
         <div
