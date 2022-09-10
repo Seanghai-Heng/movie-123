8bd20890fd42f526ceb1116b0d77562cc6b24259
Seanghai <=>
2022-09-10 19:34:50 +0700

    add dynamic view to home page

--- pages/index.vue
+++ pages/index.vue
@@ -2,166 +2,71 @@
   <div class="bg-black font-mono">
     <div class="container text-white">
       <div class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2">
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://i.pinimg.com/originals/7f/43/64/7f43643fb543aadcefb750fa43193262.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">Kin</span>
+        <!-- Tv show lists -->
+        <div v-for="result in results" :key="result.id" class="movie-list p-4">
+          <router-link :to="`/tvShows/${result.id}`">
+            <div class="test relative">
+              <div class="movie-poster overflow-hidden" v-if="result.image">
+                <img
+                  class="w-full h-full  transform hover:scale-125 duration-500"
+                  :src="result.image.medium"
+                  :alt="result.name"
+                />
+                <span class="absolute text-center">{{ result.name }}</span>
+              </div>
             </div>
-          </div>
+          </router-link>
         </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://cdn.bollywoodmdb.com/movies/largethumb/2018/malang/malang-poster-7.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">MALANG</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://i.pinimg.com/originals/03/c0/c2/03c0c2137dd12b0dd65d89c3b2ac2baa.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">Robin Hood</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://posterspy.com/wp-content/uploads/2019/06/bw-poster.png"
-                alt=""
-              />
-              <span class="absolute text-center">Black Widow</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://i.pinimg.com/originals/3f/64/80/3f64803961e5d478d4236ff9c2465156.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">No Time to Die</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Shaan_%282020%29_theatrical_release_poster.jpg/220px-Shaan_%282020%29_theatrical_release_poster.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">Shaan</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://pbs.twimg.com/media/EZ9MLUpVAAA_Yuy.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">Assasin In Red</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://i1.wp.com/geekdommovies.com/wp-content/uploads/2019/12/avatar.jpg?resize=750%2C923"
-                alt=""
-              />
-              <span class="absolute text-center">Avatar</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://media-cache.cinematerial.com/p/500x/fsx8noie/black-adam-movie-poster.jpg?v=1573836077"
-                alt=""
-              />
-              <span class="absolute text-center">Black Adam</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://m.media-amazon.com/images/M/MV5BYjk5YTUyN2QtZjhkOS00ZjllLTg3ZDktNzBiNmM2YWVhNDA4XkEyXkFqcGdeQXVyODEyNjEwMDk@._V1_.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">HEROPANTI2</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://www.teahub.io/photos/full/339-3395521_conjuring-2013-movie-poster.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">The Conjuring 2</span>
-            </div>
-          </div>
-        </div>
-        <div class="movie-list p-4">
-          <div class="test relative">
-            <div class="movie-poster overflow-hidden">
-              <img
-                class="w-full h-full  transform hover:scale-125 duration-500"
-                src="https://upload.wikimedia.org/wikipedia/en/thumb/5/51/Brahms_The_Boy_Poster.jpg/220px-Brahms_The_Boy_Poster.jpg"
-                alt=""
-              />
-              <span class="absolute text-center">Brahms the Boy</span>
-            </div>
-          </div>
-        </div>
+        <!-- End tv show lists -->
       </div>
     </div>
     <div class="pb-4"></div>
-      <div class="flex pb-5 mx-auto px-3pt-5 
+    <div
+      class="flex pb-5 mx-auto px-3pt-5 
             border-t border-gray-500 text-gray-400 text-sm 
-            flex-col md:flex-row max-w-6xl"></div>
+            flex-col md:flex-row max-w-6xl"
+    ></div>
   </div>
 </template>
 
-<script></script>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      page: 0,
+      results: [],
+      loading: true
+    };
+  },
+  methods: {
+    async tvShowInfo() {
+      await axios.get(`https://api.tvmaze.com/shows?page=${this.page}`).then(
+        response => {
+          this.results = response.data;
+          this.loading = false;
+          console.log(this.results);
+        },
+        error => {
+          this.loading = false;
+        }
+      );
+    }
+  },
+  mounted() {
+    this.tvShowInfo();
+  }
+};
+</script>
 
 <style scoped>
+a:hover{
+  color: white;
+}
 .test {
   height: 300px;
 }
-.container{
+.container {
   width: 80%;
 }
 .movie-poster {
